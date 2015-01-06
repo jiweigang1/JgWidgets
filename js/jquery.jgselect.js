48887d768626e86d9ed8854ceda99e6e383b32d8
jigang <jiweigang2008@126.com>
2015-01-06 14:43:02 +0800

    edit

--- js/jquery.jgselect.js
+++ js/jquery.jgselect.js
@@ -12,18 +12,26 @@
 (function ($) {
     $.widget("jgWidgets.jgSelect", {
         options: {
+			url:null,
+			ajaxType:"post",
             maxDropHeight:200,
+			width:260,
+			disabled: false,
+			selectionIndex:0
+			
         },
 		_create:function(){
-			this.settings = {
-				width: 260,
-				selectionIndex: 0,
-				disabled: false,
-				showSelectionTextOnly: false,
-				onSelectedOnInit: false,
-				onSelected: function (index, value, text) { }
-			};
-			this._initHtml();
+			var self = this;
+			this._initOptions();
+			this._loadOptions(function(){
+				self._initHtml();
+			});
+			
+		},
+		_initOptions:function(){
+			this.options.url 	  		  =	getValue(this.element,"url",		  this.options.url);
+			this.options.ajaxType 		  = getValue(this.element,"ajaxType",	  this.options.ajaxType);
+			this.options.maxDropHeight 	  = getValue(this.element,"maxDropHeight",this.options.maxDropHeight,"int");
 		},
 		_selectIndex: function(index) {
             var $option = this.element.find("option").eq(index)
@@ -37,15 +45,39 @@
 			this.element.trigger("change");
         },
 
+		_loadOptions:function(fn){
+			var self = this;
+			if(this.options.url){
+				$.ajax({
+					url:self.options.url,
+					type:self.options.ajaxType,
+					cache:false
+				}).done(function(data){
+					self.element.append(data);
+					if($.isFunction(fn)){
+						fn.call(self);
+					}	
+				});
+			}else{
+				if($.isFunction(fn)){
+						fn.call(self);
+				}
+			}
+		},
         _showOptions: function() {
+			var self = this;
             var $other = $('.jg-select-container .options.open').not(this.$options);
             if ($other.length > 0) {
                 $other.removeClass('open');
                 $other.slideUp(50);
             }
 			this.$arrow.removeClass("close").addClass("open");
-			this.$optionsScroll.scrollTop(0);
-            this.$optionsScroll.slideDown('fast');
+            this.$optionsScroll.slideDown('fast',function(){
+				self.$optionsScroll.scrollTop(0);
+				if($.fn.jgScrollbar){
+					self.$optionsScroll.jgScrollbar("update");
+				}
+			});
         },
 
         _closeOptions: function () {
@@ -96,16 +128,19 @@
 										 </div>\
 									  </div>');
             this.$selectContainer.insertAfter(this.element);
-			this.$selectContainer.css({width:this.settings.width})
+			this.$selectContainer.css({width:this.options.width})
 			this.$selected  = this.$selectContainer.find(".selected");
 			this.$arrow = this.$selectContainer.find(".arrow");
 			this.$optionsContainer = this.$selectContainer.find(".options-container");
-			this.$optionsContainer.css({width:this.settings.width});
+			this.$optionsContainer.css({width:this.options.width});
 			this.$options   = this.$selectContainer.find(".options");
-            this.$selected.css({ width: this.settings.width - 8});
+            this.$selected.css({ width: this.options.width - 8});
 			this.$optionsScroll = this.$selectContainer.find(".options-scroll");
 			this.$optionsScroll.css({"max-height":this.options.maxDropHeight});
-			
+			if($.fn.jgScrollbar){
+				this.$optionsScroll.css({"overflow":"hidden","position":"relative"})
+				this.$optionsScroll.jgScrollbar({autoShow:false});
+			}
            
 			var optionsHtml = "";
 			var i=0;
@@ -114,12 +149,58 @@
 				i++;
 			});
 			this.$options.html(optionsHtml);
-            if (!this.settings.disabled) {
+            if (!this.options.disabled) {
                 this._enable();
             }else {
                 this.$selected.addClass('disabled');
             } 
-            this._selectIndex(this.settings.selectionIndex);
+            this._selectIndex(this.options.selectionIndex);
         }
 	});	
+	
+	function getValue($el,name,defaultValue,type){
+		if(!type){
+			type = "string";
+		}
+		var value = $el.attr(name);
+		if(!value){
+			return defaultValue;
+		}else{
+			if(type=="string"){
+				return value;
+			}else if(type=="boolean"){
+				if(value=="true"){
+					return true;
+				}else{
+					return false;
+				}
+			}else if(type=="function"){
+				if($.isFunction(value)){
+					return value;
+				}else{
+					var v;
+					try{
+						v = eval(value)
+					}catch(e){}
+					if($.isFunction(v)){
+						return v;
+					}else{
+						return defaultValue;
+					}
+				}
+			}else if(type=="object"){
+					var v;
+					try{
+						v = eval(value)
+					}catch(e){}
+					if(v){
+						return v;
+					}else{
+						return defaultValue;
+					}
+			}
+		}
+		return defaultValue;
+	}
+	
 })(jQuery);	
